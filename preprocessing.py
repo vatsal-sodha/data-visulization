c9541c000d8991f49fbf62300e9fbefb012c8fc3
vatsal-sodha <vatsalsodha150297@gmail.com>
2017-09-23 19:45:09 +0530

    Done Preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -6,6 +6,13 @@ import re
 murderFile1="murder_data/Murder-2001-2012.csv"
 murderFile2="murder_data/Murder-2013.csv"
 murderFile3="murder_data/Murder-2015.csv"
+
+kidnappingFile1="kidnapping_data/kidnapping-2001-2012.csv"
+kidnappingFile2="kidnapping_data/kidnapping-2013.csv"
+kidnappingFile3="kidnapping_data/kidnapping-2014.csv"
+kidnappingFile4="kidnapping_data/kidnapping-2015.csv"
+
+
 def murderDataIntegration():
 	df1=pd.read_csv(murderFile1)
 	df2=pd.read_csv(murderFile2)
@@ -32,4 +39,48 @@ def murderDataIntegration():
 	print(df.shape)
 	return df
 
-murderDataIntegration()
+def kidnappingDataIntegration():
+	df1=pd.read_csv(kidnappingFile1)
+	df2=pd.read_csv(kidnappingFile2)
+	df3=pd.read_csv(kidnappingFile3)
+	df4=pd.read_csv(kidnappingFile4,encoding = "ISO-8859-1")
+
+	df1=df1[['STATE/UT','YEAR','Purpose','Grand Total']]
+	df2=df2[['STATE/UT','YEAR','Purpose','Grand Total']]
+	df3=df3[['States/UTs','Crime Head','Year','Total_Sex and Age Wise']]
+	df4=df4[['State/UT','Grand Total']]
+
+	# changed pupose to purpose in data set and preprocessing the data file 1 & 2
+	df1['Purpose']=(df1.loc[df1['Purpose']=='Total'])
+	df1=df1.dropna()
+	df1=df1.drop('Purpose',1)
+
+	df2['Purpose']=(df2.loc[df2['Purpose']=='Total'])
+	df2=df2.dropna()
+	df2=df2.drop('Purpose',1)
+
+	df3=(df3.loc[df3['Crime Head']=='2 - Total Kidnapped & abducted'])
+	df3=df3.drop(df3[df3['States/UTs'].isin(["Total (States)","Total (UTs)","Total (All India)"])].index)
+	df3=df3.dropna()
+	df3=df3.drop('Crime Head',1)
+
+	df4=df4.drop(df4[df4['State/UT'].isin(["TOTAL (STATES)","TOTAL (UTS)","TOTAL ALL INDIA"])].index)
+	df4['Year']='2015'
+	df4=df4.rename(columns={'State/UT':'State','Year':'Year','Grand Total':'Total'})
+
+	# appending the data
+	df=df1.append(df2)
+	df=df.rename(columns={'STATE/UT':'State','YEAR':'Year','Grand Total':'Total'})
+	df3=df3.rename(columns={'States/UTs':'State','Year':'Year','Total_Sex and Age Wise':'Total'})
+	df=df.append(df3)
+	df=df.append(df4)
+	print(df1.shape)
+	print(df2.shape)
+	print(df3.shape)
+	print(df4.shape)
+	print(df.shape)
+
+
+
+# murderDataIntegration()
+kidnappingDataIntegration()
